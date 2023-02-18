6e5f37bc91dfb66533943feeb11f4d2211b8782d
Max MacGillivray <mary.macgillivray@gmail.com>
2023-02-18 01:14:57 -0700

    commented out code in view that doesn't do what's intended

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     @nation = params[:nation].gsub(/[^0-9a-z]/i, ' ').split(/ |\_/).map(&:capitalize).join(" ")
-    @total_results = CharacterFacade.fire.count
+    # @total_results = CharacterFacade.fire.count
     # @members = 
   end
 end
